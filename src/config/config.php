8b7e38bcf446cac0a03da24f2ef6a1a3c8fb4093
Wimurk <wimurk@users.noreply.github.com>
2023-08-11 10:16:56 +0200

    Update config.php

--- src/config/config.php
+++ src/config/config.php
@@ -3,14 +3,6 @@
 return [
     
     /**
-     * The relative path from your root to your models location
-     * You can add multiple model paths
-     */
-    'models_path' => [
-        "app/Models"
-    ],
-    
-    /**
      * The relative path from your root to the location where the seeders will be generated
      */
     'output_path' => "database/seeders",
@@ -19,7 +11,7 @@ return [
      * These columns will be excluded from your seeders
      */
     'exclude_columns' => [
-        "id"
+        
     ],
     
     
